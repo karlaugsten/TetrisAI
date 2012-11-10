ce61631c3788e56d6cee42a9e74068aacd62c7d8
Karl Augsten <karlaugsten@gmail.com>
2012-11-10 15:00:26 -0700

    Working on adding system responses to the communicator to process
    Careful! not in working state

---
+++ TetrisAI/AI/src/tetrisconnector/BoardStateResp.java
@@ -0,0 +1,13 @@
+package tetrisconnector;
+
+import org.json.JSONObject;
+
+public class BoardStateResp extends Response {
+	
+	public BoardStateResp(JSONObject j){
+		
+		super(1,2.0,"");
+		
+	}
+
+}
---
+++ TetrisAI/AI/src/tetrisconnector/Response.java
@@ -0,0 +1,16 @@
+package tetrisconnector;
+
+import org.json.JSONObject;
+
+public class Response {
+	
+	private int sequence;
+	private double timestamp;
+	private String type;
+	private JSONObject message;
+	
+	public Response(int sequence, double timestamp, String type){
+		
+		
+	}
+}
